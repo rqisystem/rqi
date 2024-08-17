875f840e1ecaab3dd56684d0698e27326dbab0a8
rqisystem <qudret.rustem@inbox.ru>
2024-08-17 17:45:38 +0400

    commit

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -5,7 +5,7 @@ const Certificates = () => {
     <div
       className="p-4 rounded-md mb-2"
       style={{ background: 'linear-gradient(135deg, #63EEC5, #A12699)' }}>
-      <h2 className="text-white text-xl font-bold mt-6 mb-4">Sertifaklar</h2>
+      <h2 className="text-white text-xl font-bold mt-6 mb-4">Sertifikatlar</h2>
       {Object.keys(certificates).map((category) => (
         <div key={category} className="mb-6">
           <h3 className="text-white text-lg font-semibold mb-2 capitalize">{category}</h3>
